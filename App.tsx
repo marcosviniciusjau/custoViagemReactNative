9561ad5b33c78312966422a5e651593d989ab9a7
Marcos Vinicius Araujo da Silva <marcosviniciusjahu@gmail.com>
2024-01-24 12:41:44 -0300

    primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from "native-base"
+import { Routes } from '@routes/index';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  <NativeBaseProvider>
+   <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent>
+   </StatusBar>
+   <Routes/>
+  </NativeBaseProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
